a662b53f66b0454bc46a8eae924a5529c6183127
Emiliano Rodriguez <rodr1guezemi@Emilianos-MacBook-Air.local>
2023-10-07 16:30:06 -0600

    Daily leet code: Is Palindrome (#9)(10/7/23)

---
+++ src/PalindromeNumber/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+}
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        StringBuilder s = new StringBuilder(String.valueOf(x));
+        s.reverse();
+
+        return s.toString().equals(x + "");
+    }
+}
